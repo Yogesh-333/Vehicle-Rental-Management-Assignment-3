e11246a838be8b5a61a36aceca9b2b80ac5d0eb0
Yogesh Kumar <yogeshgopal98@gmail.com>
2024-07-16 21:58:05 -0400

    Basic Vehicle Class

--- Vehicle Rental Management/Vehicle Rental Management/Program.cs
+++ Vehicle Rental Management/Vehicle Rental Management/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Vehicle
+{
+    // Properties
+    private string model;
+    private string manufacturer;
+    private int year;
+    private double rentalPrice;
+
+    // Get and Set methods for properties
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+    public string Manufacturer
+    {
+        get { return manufacturer; }
+        set { manufacturer = value; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    public double RentalPrice
+    {
+        get { return rentalPrice; }
+        set { rentalPrice = value; }
+    }
+
+    // Method to display vehicle details
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: ${RentalPrice}");
+    }
+}
